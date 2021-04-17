659baa4a0715b7b76ea00465f90b8d1042e6eca1
Aaron Williams <aaronrw4@yahoo.com>
2021-04-17 01:43:35 -0700

    Updates

---
+++ precache-manifest.7322554fbd479bb6b9dbff74018c752a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf32b02cae1af1fc72da3671b09ce78",
+    "url": "/team-builder/index.html"
+  },
+  {
+    "revision": "b8668318c0de40504500",
+    "url": "/team-builder/static/css/main.5c6436d7.chunk.css"
+  },
+  {
+    "revision": "7413db4e60c3f7bf5881",
+    "url": "/team-builder/static/js/2.922a1696.chunk.js"
+  },
+  {
+    "revision": "b8668318c0de40504500",
+    "url": "/team-builder/static/js/main.b60ccad1.chunk.js"
+  },
+  {
+    "revision": "788fd4cf338e34873dcd",
+    "url": "/team-builder/static/js/runtime-main.5607b09c.js"
+  }
+]);
